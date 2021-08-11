5b95cfca3b20777e9eef0f25e1017250372f2f29
Emmanuel Labal <emmanuel.labal@gmail.com>
2021-08-10 22:47:20 -0300

    Initial commit
    Collecting locators for home page: Logo, search box and search submit button

---
+++ Test/PageObjects/Locators/Home.py
@@ -0,0 +1,6 @@
+
+class Locator(object):
+
+    amazon_logo = "//a[@id='nav-logo-sprites']"
+    search_box = "//input[@id='twotabsearchtextbox']"
+    submit_search = "//input[@id='nav-search-submit-button']"
