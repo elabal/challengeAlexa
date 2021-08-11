d9441e698990402e84d94140700e68981c679353
Emmanuel Labal <emmanuel.labal@gmail.com>
2021-08-11 01:54:31 -0300

    -Refactoring header locator, due to language switch. Removing hardcoded english text -Refactoring product locator, due to div not being clickable. Refactoring to focus con //h2//a child which is clickable

--- Test/PageObjects/Locators/SearchResults.py
+++ Test/PageObjects/Locators/SearchResults.py
@@ -1,7 +1,7 @@
 
 class Locator(object):
 
-    header_results_for_searched_item = "//span[contains(text(),'results for')]/following-sibling::span[contains(text(), '{VALUE}')]]"
+    header_results_for_searched_item = "//span[contains(text(),'')]/following-sibling::span[contains(text(), '{VALUE}')]"
     button_page = "//a[text()='{VALUE}']"
-    product_by_index = "//div[@data-index='{VALUE}']"
+    product_by_index = "//div[@data-index='{VALUE}']//h2//a"
     
