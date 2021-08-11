d4edb77d7f8590559ba7aca1dc3741381c10cdd5
Emmanuel Labal <emmanuel.labal@gmail.com>
2021-08-11 01:55:07 -0300

    Refactoring assertion using exception handling

--- Test/PageObjects/Pages/PDP.py
+++ Test/PageObjects/Pages/PDP.py
@@ -2,6 +2,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from Test.PageObjects.Locators.PDP import Locator
+from selenium.common.exceptions import InvalidArgumentException
 
 
 class PDP(object):
@@ -11,5 +12,9 @@ class PDP(object):
         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, Locator.ppd_container)))
 
     def check_available_for_purchase(self):
-        assert self.driver.find_element(Locator.add_to_cart) in self.driver
+        try:
+            add_to_cart = self.driver.find_element(Locator.add_to_cart)
+            add_to_cart.click()
+        except InvalidArgumentException:
+            raise Exception("Item is not avaiable for purchase")
 
